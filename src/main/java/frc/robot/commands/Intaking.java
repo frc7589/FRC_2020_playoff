d2674d579d672eedbce54c8e60cd59612adb0fda
sean4101 <seanchen1014@gmail.com>
2020-10-27 20:58:58 +0800

    Shooting stuff

--- src/main/java/frc/robot/commands/Intaking.java
+++ src/main/java/frc/robot/commands/Intaking.java
@@ -11,14 +11,24 @@ public class Intaking extends CommandBase {
     private final IntakeSubsystem m_intakeSubsystem;
     private final BooleanSupplier m_toggleIntake;
     private final BooleanSupplier m_toggleArm;
+    private final BooleanSupplier m_spin;
     private boolean arm_out = false;
     private boolean intake_enabled = false;
+    private boolean spinning = false;
+    private double delayTimer = 0;
+    private double spinTimer = 0; 
+
+    private double prev_time;
+    private double cur_time;
+    private double deltaTime;
 
 
-    public Intaking(IntakeSubsystem intakeSubsystem, BooleanSupplier toggleIntake, BooleanSupplier toggleArm) {
+    public Intaking(IntakeSubsystem intakeSubsystem, BooleanSupplier toggleIntake,
+                    BooleanSupplier toggleArm, BooleanSupplier spinPressed) {
         m_intakeSubsystem = intakeSubsystem;
         m_toggleIntake = toggleIntake;
         m_toggleArm = toggleArm;
+        m_spin = spinPressed;
         
         addRequirements(m_intakeSubsystem);
     }
@@ -27,10 +37,16 @@ public class Intaking extends CommandBase {
     public void initialize() {
         arm_out = false;
         intake_enabled = false;
+        cur_time = System.currentTimeMillis();
+        prev_time = System.currentTimeMillis();
+        deltaTime = 0;
     }
 
     @Override
     public void execute() {
+        cur_time = System.currentTimeMillis();
+        deltaTime = cur_time - prev_time;
+
         if (m_toggleArm.getAsBoolean()) arm_out = !arm_out;
         if (arm_out) {
             m_intakeSubsystem.leftArm.set(Value.kForward);
@@ -48,5 +64,29 @@ public class Intaking extends CommandBase {
         else {
             m_intakeSubsystem.intaker.set(0);
         }
+
+        if (m_spin.getAsBoolean()) {
+            if (spinning) {
+                spinTimer += deltaTime/1000;
+                m_intakeSubsystem.spin.set(Constants.kSpinCylinderSpeed);
+            }
+            else {
+                delayTimer += deltaTime/1000;
+                m_intakeSubsystem.spin.set(0);
+            }
+            if (spinTimer > Constants.kSpinDurration) {
+                spinning = false;
+                spinTimer = 0;
+            }
+            else if (delayTimer > Constants.kSpinDelay) {
+                spinning = true;
+                delayTimer = 0;
+            }
+        }
+        else {
+            m_intakeSubsystem.spin.set(0);
+        }
+
+        prev_time = cur_time;
     }
 }
